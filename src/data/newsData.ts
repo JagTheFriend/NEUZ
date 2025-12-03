1485cae9b5b129f14a054691f8b455f2b7267f96
JagTheFriend <JagTheFriend12@gmail.com>
2025-12-03 17:44:06 +0530

    Use API to fetch data
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/data/newsData.ts
+++ src/data/newsData.ts
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 export type NewsArticle = {
 	id: number;
 	title: string;
@@ -6,6 +8,20 @@ export type NewsArticle = {
 	sourceUrl: string;
 };
 
+const API_URL =
+	"https://api.spaceflightnewsapi.net/v4/articles/?format=json&limit=20";
+
 export async function getNewsArticles(): Promise<NewsArticle[]> {
-	return [];
+	const response = await axios.get(API_URL);
+	if (response.status === 200) {
+		return response.data.results.map((d: any) => ({
+			id: d.id,
+			title: d.title,
+			summary: d.summary,
+			imageUrl: d.image_url,
+			sourceUrl: d.url,
+		}));
+	} else {
+		throw new Error("Failed to fetch news articles");
+	}
 }
