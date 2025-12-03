b350fd89aec2a869eed41c6e86e1afd1c833a92e
JagTheFriend <JagTheFriend12@gmail.com>
2025-12-03 17:34:36 +0530

    Working on loading data from API
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,35 @@
+import { Activity, useEffect, useState } from "react";
 import Header from "@/components/Header";
 import NewsGrid from "@/components/NewsGrid";
-import { newsArticles } from "@/data/newsData";
+import Skeletons from "./components/Skeletons";
+import { getNewsArticles, type NewsArticle } from "./data/newsData";
 
 const Index = () => {
+	const [newsArticles, setNewsArticle] = useState<NewsArticle[]>([]);
+	const [isLoading, setIsLoading] = useState(true);
+
+	useEffect(() => {
+		const fetchNewsArticles = async () => {
+			const articles = await getNewsArticles();
+			setNewsArticle(articles);
+			setIsLoading(false);
+		};
+		fetchNewsArticles();
+	}, []);
+
 	return (
 		<div className="min-h-screen bg-background">
 			<Header />
 
-			<main className="container mx-auto px-4 py-8">
-				<NewsGrid articles={newsArticles} />
-			</main>
+			<Activity mode={isLoading ? "visible" : "hidden"}>
+				<Skeletons />
+			</Activity>
+
+			<Activity mode={isLoading ? "hidden" : "visible"}>
+				<main className="container mx-auto px-4 py-8">
+					<NewsGrid articles={newsArticles} />
+				</main>
+			</Activity>
 
 			<footer className="border-t border-border py-6">
 				<div className="container mx-auto px-4 text-center">
